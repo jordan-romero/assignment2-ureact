271859d01346dae41db0bad295d665d0ac5bba63
jordles113 <jordan.t.romero@gmail.com>
2021-02-01 14:48:53 -0600

    adds style to char

--- src/components/Char.js
+++ src/components/Char.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const Char = () => {
+    const style = {
+        display: 'inline-block',
+        padding: '16px',
+        textAlign: 'center',
+        margin: '16px',
+        border: '1px solid black'
+
+    }
     return (
         <div>
             
